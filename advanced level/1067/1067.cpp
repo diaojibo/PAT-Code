915868081a958efecd8b570595fa058ee0246b87
diaojibo <522250848@qq.com>
2016-11-18 00:25:54 +0800

    update

--- advanced level/1067/1067.cpp
+++ advanced level/1067/1067.cpp
@@ -1,40 +1,47 @@
 #include<iostream>
 #include<stdio.h>
+#include<stack>
 using namespace std;
 
 #define N 100000
 int a[N],pos[N];
-int n,m;
+bool mright[N];
+int n,m,rp=0;
+stack<int> wrong;
 int tol = 0;
 void mswap(int x,int y){
 	int t;
 	tol++;
+	if(a[x]==y) rp++,mright[y]=true;
+	else mright[a[x]]=false,wrong.push(a[x]);
+	if(a[y]==x) rp++,mright[x]=true;
+	else mright[a[y]]=false,wrong.push(a[y]);
 	pos[a[x]] = y;
 	pos[a[y]] = x;
 	t = a[x];
 	a[x] = a[y];
 	a[y] = t;
 }
-bool judge(){
-	for(int i=0;i<n;i++){
-		if(a[i]!=i) return false; 
-	}
-	return true;
-}
+
 int main(){
 	scanf("%d",&n);
-	cout<<n<<endl;
+	//cout<<n<<endl;
 	for(int i=0;i<n;i++){
 		scanf("%d",&a[i]);
 		pos[a[i]] = i;
+		if(a[i]==i) rp++,mright[i]=true;
+		else wrong.push(a[i]),mright[i]=false;
 	}
-	while(!judge()){
+	while(rp<n){
 		//for(int i=0;i<n;i++) cout<<a[i]<<" ";
-		cout<<endl;
+		//cout<<endl;
 		if(a[0]==0){
+			rp--;
 			int j = 0;
-			while(a[j]==j) j++;
-			mswap(pos[0],pos[j]);
+			while(!wrong.empty()&&(wrong.top()==0||mright[wrong.top()])) wrong.pop();
+			int w = wrong.top();
+			wrong.pop();
+			mswap(pos[0],pos[w]);
 		}
 		mswap(pos[0],pos[pos[0]]);
 	}
